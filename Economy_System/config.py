d50ead0774b5c100647345c8635b543e2653b23a
Wilcon <166444317+wilcon1@users.noreply.github.com>
2024-04-25 21:08:11 +0200

    Update config.py

--- Economy_System/config.py
+++ Economy_System/config.py
@@ -1,6 +1,6 @@
-TOKEN = "MTIxNjg2NDA4MDU0ODA3MzYzMw.Glr8QG.T6BzXZLD-G-xvsvGxhQ9nmxn-dV5gK9AOtiteQ" #your bot token
-db_user = "postgres" #*database username
-db_pass = "ahmedelpop123" #**database password
-db_host = "localhost" #*database host
-db_name = "postgres" #*database name
-db_port = "5432" #*database port
+TOKEN = "" #your bot token
+db_user = "" #*database username
+db_pass = "" #**database password
+db_host = "" #*database host
+db_name = "" #*database name
+db_port = "" #*database port
